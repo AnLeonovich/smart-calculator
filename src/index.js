e98cc6861d767f17e04a47c4925fb63f73845ddd
AnLeonovich <novka666@mail.ru>
2018-03-02 01:58:07 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,27 +1,50 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.counter = 0;
+    this.math = '' + initialValue;
+    this.help = '' + initialValue; 
   }
+  
 
   add(number) {
-    // your implementation
+    this.counter = 0;
+    this.help = number; 
+    this.math = this.math+'+'+number;
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.counter = 0;
+    this.help = number;
+    this.math = this.math+'-'+number;
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.counter = 0;
+    this.help = number;
+    this.math = this.math+'*'+number;
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.counter = 0;
+    this.help = number;
+    this.math = this.math+'/'+number;
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.counter += 1;
+    this.math = this.math.slice(0, this.math.lastIndexOf(this.help));  
+    this.math = this.math+'Math.pow('+this.help + ',' + number + new Array(this.counter+1).join(")");
+    this.help = number;
+    return this;
   }
+
+  toString(){
+     return eval(this.math); 
+    }
 }
 
 module.exports = SmartCalculator;
